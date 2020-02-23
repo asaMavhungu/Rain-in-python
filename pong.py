c92a8efc3bf97fe2afe0b6d046e1fde44e43b364
Asakundwi Mavhungu <52137515+Mavhasa17@users.noreply.github.com>
2020-02-23 22:38:35 +0200

    Add files via upload

---
+++ pong.py
@@ -0,0 +1,37 @@
+# Import and init
+import pygame
+pygame.init()
+pygame.mixer.init()
+
+# define colors
+black = (0, 0, 0)
+white = (255, 255, 255)
+
+# Rain coordinates
+x = 0
+y = 5
+# open new window
+size = (700, 500)
+screen = pygame.display.set_mode(size)
+pygame.display.set_caption("Real True Work of art")
+
+carryOn = True
+clock = pygame.time.Clock()
+
+# Main loop
+while carryOn:
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			carryOn = False
+
+	# Game logic
+	# make screen black
+
+	screen.fill(black)
+	pygame.draw.line(screen, white, (349, x), (349, y), 5)
+	x += 1
+	y += 1
+
+	pygame.display.flip()
+
+pygame.quit()
