f365cba2789a56b5df7a1986c836a49ef88964cf
Asakundwi Mavhungu <52137515+Mavhasa17@users.noreply.github.com>
2020-07-21 00:01:29 +0200

    Add files via upload

--- rain.py
+++ rain.py
@@ -1,42 +1,54 @@
-# Import and init
 import pygame
-pygame.init()
-pygame.mixer.init()
-# define colors
-black = (0, 0, 0)
-white = (255, 255, 255)
+
+resolution = (640, 360)
+
+WINDOW = pygame.display.set_mode(resolution)
+
+BLUE = (102, 153, 255)
+BLUE_DARK = (0, 0, 153)
+
+class Drop(object):
+	def __init__(self, x, y):
+		self.x = x
+		self.y = y
+		self.yspeed = 0.05
+
+	def fall(self):
+		self.y += self.yspeed
+
+	def show(self):
+		self.y = self.y
+		pygame.draw.rect(WINDOW, BLUE_DARK, [self.x, self.y, 2, 9])
 
 
-#added new line
-# Rain coordinates
-x = 0
-y = 5
-# open new window
-size = (700, 500)
-screen = pygame.display.set_mode(size)
-pygame.display.set_caption("Real True Work of art")
 
-carryOn = True
-clock = pygame.time.Clock()
 
-if 1 ==1:
-	pass
-# Main loop
-while carryOn:
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT:
-			carryOn = False
 
-	# Game logic
-	if 1==2:
-		pass
-	# make screen black
 
-	screen.fill(black)
-	pygame.draw.line(screen, white, (349, x), (349, y), 5)
-	x += 1
-	y += 1
 
-	pygame.display.flip()
 
-pygame.quit()
+def main():
+
+	d = Drop(640/2 , 360/2)
+
+	drops = [Drop(i, 10) for i in range(0, 640, 10)]
+
+	running = True
+	while running:
+
+		# Did the user click the window close button?
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				running = False
+
+		# Fill the background with white
+		WINDOW.fill(BLUE)
+
+		for x in drops:
+			x.fall()
+			x.show()
+
+	    # Flip the display
+		pygame.display.flip()
+
+main()
